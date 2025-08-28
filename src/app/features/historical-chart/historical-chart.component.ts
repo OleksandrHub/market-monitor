c02384a76c983d08aed516f8e3654a10e23cca2b
OleksandrHub <sasha.klishch1@gmail.com>
2025-08-28 13:15:32 +0300

    feat: implement new app structure
    - Created app component
    - Added core and features modules
    - Implemented basic components
    - Added model for assets and prices
    - Created services for realtime and historical data

---
+++ src/app/features/historical-chart/historical-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historical-chart',
+  imports: [],
+  templateUrl: './historical-chart.component.html',
+  styleUrl: './historical-chart.component.scss'
+})
+export class HistoricalChartComponent {
+
+}
---
+++ src/app/features/live-price/live-price.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-price',
+  imports: [],
+  templateUrl: './live-price.component.html',
+  styleUrl: './live-price.component.scss'
+})
+export class LivePriceComponent {
+
+}
