2d179aeb7af89a716c12a68fd74332a1abdaf321
OleksandrHub <sasha.klishch1@gmail.com>
2025-08-29 14:42:03 +0300

    feat: implement dashboard page
    - Added dashboard component
    - Created live price & chart sections
    - Implemented currency selection
    - Designed UI with SCSS
    - Integrated child components

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HistoricalChartComponent } from '../historical-chart/historical-chart.component';
+import { LivePriceComponent } from '../live-price/live-price.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [HistoricalChartComponent, LivePriceComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
